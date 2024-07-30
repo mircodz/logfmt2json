24ab9eb671b2f67f386aca95df408be7e3eaf2e4
Mirco De Zorzi <mirco.dezorzi@sysdig.com>
2024-07-30 19:32:35 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mircodz/logfmt2json
+
+go 1.22.1
+
+require github.com/go-logfmt/logfmt v0.6.0
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"os"
+
+	"github.com/go-logfmt/logfmt"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	scanner := bufio.NewScanner(reader)
+
+	for scanner.Scan() {
+		s := scanner.Bytes()
+		decoder := logfmt.NewDecoder(bytes.NewReader(s))
+
+		o := make(map[string]string)
+		for decoder.ScanRecord() {
+			for decoder.ScanKeyval() {
+				o[string(decoder.Key())] = string(decoder.Value())
+			}
+		}
+
+		bs, err := json.Marshal(o)
+		if err != nil {
+			continue
+		}
+
+		if _, err := os.Stdout.Write(bs); err != nil {
+			continue
+		}
+	}
+}
